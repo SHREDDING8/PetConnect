68dd81baf97b028abbec472966062aa91e5df946
Egor Zavrazhnov <zavrazhnov.zhora@mail.ru>
2023-08-12 15:43:10 +0300

    created Builder Created TabBar

---
+++ PetConnect/Modules/MainBuilder.swift
@@ -0,0 +1,45 @@
+//
+//  MainBuilder.swift
+//  PetConnect
+//
+//  Created by SHREDDING on 12.08.2023.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - MainBuilderProtocol
+protocol MainBuilderProtocol{
+    static func createMainBuilder() -> UIViewController
+    
+    static func createHomePage()->UIViewController
+    static func createPetPage()->UIViewController
+    static func createWalkPage()->UIViewController
+    static func createProfilePage()->UIViewController
+}
+
+// MARK: - MainBuilder
+class MainBuilder:MainBuilderProtocol{
+    static func createMainBuilder() -> UIViewController{
+        let tabBar = MainTabBarViewController()
+        return tabBar
+    }
+    
+    // MARK: - Main Pages
+    static func createHomePage()->UIViewController{
+        return UINavigationController()
+    }
+    
+    static func createPetPage()->UIViewController{
+        return UINavigationController()
+    }
+    
+    static func createWalkPage()->UIViewController{
+        return UINavigationController()
+    }
+    
+    static func createProfilePage()->UIViewController{
+        return UINavigationController()
+    }
+    
+}
