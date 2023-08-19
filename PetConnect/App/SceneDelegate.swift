3698e8cb95c62506126929c776f7ffa48275fd50
Egor Zavrazhnov <zavrazhnov.zhora@mail.ru>
2023-08-19 15:46:24 +0300

    fix

--- PetConnect/App/SceneDelegate.swift
+++ PetConnect/App/SceneDelegate.swift
@@ -26,7 +26,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         self.window = UIWindow(windowScene: windowScene)
-        window?.rootViewController =  MainBuilder.createAuth()
+        window?.rootViewController =  rootViewController
         window?.makeKeyAndVisible()
         
         
