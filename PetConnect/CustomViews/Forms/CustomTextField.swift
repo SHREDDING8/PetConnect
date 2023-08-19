700bb87f4085e1d45d5db6f710adc3c7d52aa9a7
Egor Zavrazhnov <zavrazhnov.zhora@mail.ru>
2023-08-19 14:28:35 +0300

    custom textField view

--- PetConnect/CustomViews/Forms/CustomTextField.swift
+++ PetConnect/CustomViews/Forms/CustomTextField.swift
@@ -12,24 +12,53 @@ protocol CustomTextFieldDelegate{
     
 }
 
-class CustomTextField: UIView {
+@IBDesignable
+open class CustomTextField: UIView {
+    
+    let nibName = "CustomTextField"
     
     var delegate:CustomTextFieldDelegate?
     
+    @IBOutlet weak var contentView: UIView!
+    
+    
     // MARK: - Objects
     
-    private lazy var textField: UITextField = {
-        let textField = UITextField()
-        
-        return textField
-    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        initSubviews()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        initSubviews()
+    }
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        initSubviews()
+    }
+    
+    func initSubviews() {
+        
+        let bundle = Bundle.init(for: CustomTextField.self)
+        
+        let view = bundle.loadNibNamed(nibName, owner: self)
+        let contentView = view?.first as! UIView
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: self.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        ])
+        
     }
     
 }
