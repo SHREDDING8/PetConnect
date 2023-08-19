fafdafa17c9d69386cc5b622f2bc4e0044b25e52
Egor Zavrazhnov <zavrazhnov.zhora@mail.ru>
2023-08-19 21:05:19 +0300

    updated custom textField

--- PetConnect/CustomViews/Forms/CustomTextField.swift
+++ PetConnect/CustomViews/Forms/CustomTextField.swift
@@ -11,6 +11,8 @@ import UIKit
 @objc protocol CustomTextFieldDelegate{
     @objc optional func textFieldDidChange()
     @objc optional func textFieldShouldReturn()
+    @objc optional func textFieldDidEndEditing()
+    @objc optional func textFieldDidBeginEditing()
 }
 
 @IBDesignable
@@ -21,13 +23,13 @@ open class CustomTextField: UIView {
     var delegate:CustomTextFieldDelegate?
     
     // MARK: - @IBOutlets
-    @IBOutlet private weak var contentView: UIView!
+    private weak var contentView: UIView!
     
     @IBOutlet private weak var borderView: UIView!
     
     @IBOutlet private weak var upperLabelView: UIView!
     
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet private weak var textField: UITextField!
     
     @IBOutlet private weak var upperLabel: UILabel!
     
@@ -35,12 +37,30 @@ open class CustomTextField: UIView {
     
     // MARK: - Properties
     
-    open var cornerRadius: CGFloat {
-        get {
-            return self.borderView.layer.cornerRadius
+    @IBInspectable
+    open var cornerRadius: CGFloat = 0 {
+        didSet{
+            self.borderView.layer.cornerRadius = cornerRadius
         }
-        set {
-            self.borderView.layer.cornerRadius = newValue
+    }
+    
+    @IBInspectable
+    open var placeholder:String = "" {
+        didSet{
+            self.textField.placeholder = placeholder
+        }
+    }
+    @IBInspectable
+    open var upperText:String = ""{
+        didSet{
+            self.upperLabel.text = upperText
+        }
+    }
+    
+    @IBInspectable
+    open var supportingText:String = ""{
+        didSet{
+            self.supportingLabel.text = supportingText
         }
     }
     
@@ -53,6 +73,7 @@ open class CustomTextField: UIView {
             self.textField.backgroundColor = newValue
         }
     }
+    
     open var textColor:UIColor = .black
     open var focusedColor:UIColor? = UIColor(named: "GreetingGreen")
     open var wrongValueColor:UIColor? = UIColor(named: "WrongValue")
@@ -60,64 +81,56 @@ open class CustomTextField: UIView {
     open var showUpperLabelWhenTextFieldIsEmpty:Bool = false
     open var showUpperLabelWhenTextFieldIsNotEmty:Bool = true
     
-    open var placeholder:String = "" {
-        didSet{
-            self.textField.placeholder = placeholder
-        }
-    }
-    open var upperText:String = ""{
-        didSet{
-            self.upperLabel.text = upperText
-        }
-    }
-    
-    open var supportingText:String = ""{
-        didSet{
-            self.supportingLabel.text = supportingText
-        }
-    }
-    
-    init(cornerRadius:CGFloat){
+    init(cornerRadius:CGFloat, upperText:String, textFieldPlaceholder:String, supportingText:String){
         super.init(frame: CGRectZero)
         self.cornerRadius = cornerRadius
-        self.viewInit()
+        self.upperText = upperText
+        self.placeholder = textFieldPlaceholder
+        self.supportingText = supportingText
+        self.commonInit()
     }
     
     // MARK: - Inits
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.translatesAutoresizingMaskIntoConstraints = false
-        viewInit()
+        commonInit()
     }
     
     required public init?(coder: NSCoder) {
         super.init(coder: coder)
         self.translatesAutoresizingMaskIntoConstraints = false
-        viewInit()
+        commonInit()
     }
     
     open override func awakeFromNib() {
         super.awakeFromNib()
-        viewInit()
+        commonInit()
     }
     
-    private func viewInit(){
-        initSubviews()
+    // MARK: - Funcs during Init
+    private func commonInit(){
+        self.contentView = initFromNib()
+        setSubviews()
+        setConstraints()
+        setInspectableValues()
         configureBorder()
         textFieldInit()
+        configureUpperLabel()
     }
-    
-    private func initSubviews() {
-        
+    private func initFromNib()->UIView{
         let bundle = Bundle.init(for: CustomTextField.self)
-        
         let view = bundle.loadNibNamed(nibName, owner: self)
         let contentView = view?.first as! UIView
         
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        
+        contentView.frame = bounds
+        return contentView
+    }
+    private func setSubviews(){
         addSubview(contentView)
-        
+    }
+    private func setConstraints(){
         NSLayoutConstraint.activate([
             contentView.topAnchor.constraint(equalTo: self.topAnchor),
             contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
@@ -125,17 +138,28 @@ open class CustomTextField: UIView {
             contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
         ])
     }
-    
+    private func setInspectableValues(){
+        self.borderView.layer.cornerRadius = self.cornerRadius
+        self.upperLabel.text = upperText
+        self.supportingLabel.text = supportingText
+        self.textField.placeholder = placeholder
+    }
     private func configureBorder(){
         self.borderView.layer.borderColor = UIColor.black.cgColor
         self.borderView.layer.borderWidth = 1
-        self.borderView.layer.cornerRadius = cornerRadius
-//        self.borderView.layer.masksToBounds = true
+        self.borderView.layer.masksToBounds = true
     }
-    
     private func textFieldInit(){
         self.textField.delegate = self
         self.textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        self.textField.tintColor = .black
+    }
+    private func configureUpperLabel(){
+        if showUpperLabelWhenTextFieldIsEmpty{
+            self.upperLabelView.layer.opacity = 1
+        }else{
+            self.upperLabelView.layer.opacity = 0
+        }
     }
     
     
@@ -147,14 +171,22 @@ open class CustomTextField: UIView {
     
     func showUpperText(){
         UIView.animate(withDuration: 0.3) {
-            self.upperLabel.layer.opacity = 1
+            self.upperLabelView.layer.opacity = 1
         }
     }
     
     func hideUpperText(){
         UIView.animate(withDuration: 0.3) {
-            self.upperLabel.layer.opacity = 0
+            self.upperLabelView.layer.opacity = 0
         }
+    }
+    
+    func setWrongValue(){
+        self.borderView.layer.borderColor = self.wrongValueColor?.cgColor
+        self.upperLabel.textColor = self.wrongValueColor
+        self.supportingLabel.textColor = self.wrongValueColor
+        self.textField.tintColor = self.wrongValueColor
+        
     }
 }
 
@@ -164,16 +196,27 @@ extension CustomTextField:UITextFieldDelegate{
         
         self.borderView.layer.borderColor = textColor.cgColor
         self.upperLabel.textColor = textColor
+        self.supportingLabel.textColor = textColor
+        
+        if !self.showUpperLabelWhenTextFieldIsEmpty && !textField.hasText{
+            self.hideUpperText()
+        }
         
         if !self.showUpperLabelWhenTextFieldIsNotEmty && textField.hasText{
             self.hideUpperText()
         }
+        
+        setWrongValue()
+        
+        delegate?.textFieldDidEndEditing?()
     }
     
     public func textFieldDidBeginEditing(_ textField: UITextField) {
         self.showUpperText()
         self.borderView.layer.borderColor = focusedColor?.cgColor
         self.upperLabel.textColor = focusedColor
+        
+        delegate?.textFieldDidBeginEditing?()
     }
     
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
