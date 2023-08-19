064768bdcb24c327720ff7c7f2e6ba3a530b5870
Alyona <maksalena04@mail.ru>
2023-08-20 00:47:07 +0300

    Check NotificationViewController

--- PetConnect/Modules/HomePage/View/NotificationViewController.swift
+++ PetConnect/Modules/HomePage/View/NotificationViewController.swift
@@ -14,6 +14,9 @@ class NotificationViewController: UIViewController {
     var presenter: NotificationPresenterProtocol?
     var ready: Bool = true
     
+    let name = CustomTextField(cornerRadius: 28, upperText: "Название*", textFieldPlaceholder: "Название*", supportingText: "")
+    let date = CustomTextField(cornerRadius: 28, upperText: "Дата окончания*", textFieldPlaceholder: "Дата окончания*", supportingText: "")
+    
     private lazy var saveButton: UIButton = {
         let button = UIButton()
         button.setTitle("Сохранить", for: .normal)
@@ -40,6 +43,8 @@ class NotificationViewController: UIViewController {
     // MARK: - Actions
     func setUpView() {
         view.addSubview(saveButton)
+        view.addSubview(name)
+        //view.addSubview(date)
         NSLayoutConstraint.activate(staticConstraints())
     }
     
@@ -49,7 +54,14 @@ class NotificationViewController: UIViewController {
         constraints.append(contentsOf: [
             saveButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             saveButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 61)
+            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 61),
+            name.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            name.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            name.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20),
+            name.heightAnchor.constraint(equalToConstant: 80),
+//            date.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 16),
+//            date.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            date.heightAnchor.constraint(equalToConstant: 80),
             ])
         
         return constraints
